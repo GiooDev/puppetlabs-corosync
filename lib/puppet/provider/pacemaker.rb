e4c9d1e21ff0d580b09f028f676b91a26dede5b5
Julien Georges <julien.georges@atos.net>
2015-12-16 16:20:42 +0100

    Adding functionnality to print stderr on failed resources.

--- lib/puppet/provider/pacemaker.rb
+++ lib/puppet/provider/pacemaker.rb
@@ -10,13 +10,13 @@ class Puppet::Provider::Pacemaker < Puppet::Provider
     if Puppet::PUPPETVERSION.to_f < 3.4
       raw, status = Puppet::Util::SUIDManager.run_and_capture(pcs_cmd)
     else
-      raw = Puppet::Util::Execution.execute(pcs_cmd, {:failonfail => failonfail})
+      raw = Puppet::Util::Execution.execute(pcs_cmd, {:failonfail => failonfail, :combine => true})
       status = raw.exitstatus
     end
     if status == 0 or failonfail == false
       return raw, status
     else
-      fail("command #{pcs_cmd.join(" ")} failed")
+      fail("command #{pcs_cmd.join(" ")} failed: #{raw}")
     end
   end
 
